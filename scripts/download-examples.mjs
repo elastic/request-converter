af9e49cf20df034249c6d36eb01589e7209fea1b
Miguel Grinberg <miguel.grinberg@gmail.com>
2024-06-11 11:28:05 +0100

    version-aware example downloader

--- scripts/download-examples.mjs
+++ scripts/download-examples.mjs
@@ -2,10 +2,21 @@ import fs from 'fs';
 import { Readable } from 'stream';
 import { finished } from 'stream/promises';
 
-const packageJson = JSON.parse(fs.readFileSync('./package.json'));
-const majorMinorVersion = packageJson.version.split(".").slice(0, 2).join(".")
-const EXAMPLES_URL =`https://raw.githubusercontent.com/elastic/built-docs/master/raw/en/elasticsearch/reference/${majorMinorVersion}/alternatives_report.json`;
+function getExamplesUrl(version) {
+  return `https://raw.githubusercontent.com/elastic/built-docs/master/raw/en/elasticsearch/reference/${version}/alternatives_report.json`;
+}
 
-const response = await fetch(EXAMPLES_URL);
+const packageJson = JSON.parse(fs.readFileSync('./package.json'));
+const majorMinorVersion = packageJson.version.split(".").slice(0, 2).join(".");
+let version = majorMinorVersion;
+let response = await fetch(getExamplesUrl(version));
+if (response.status === 404) {
+  version = 'current';
+  response = await fetch(getExamplesUrl(version));
+}
+if (response.status !== 200) {
+  throw new Error('Cannot download examples');
+}
+console.log(`Downloaded "${version}" examples for version ${majorMinorVersion}.`);
 const fileStream = fs.createWriteStream('.examples.json');
 await finished(Readable.fromWeb(response.body).pipe(fileStream));
