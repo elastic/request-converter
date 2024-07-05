5ecfa20c17cca8905103dd4dd5896135b7437d34
Miguel Grinberg <miguel.grinberg@gmail.com>
2024-07-05 19:28:53 +0100

    release script improvements

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
+# make sure there are no uncommitted changes
+if [[ $(git status -uno --porcelain) != "" ]]; then
+    echo "There are uncommitted changes in the repository."
+    echo "Please commit or stash them before running this script."
+    echo
+    git status
+    exit 1
+fi
+
+# get the release branch
 BRANCH=$(git branch --show-current)
 if [[ "$BRANCH" == "main" ]]; then
     echo "Must be run from a release branch!"
     exit 1
 fi
 
+# make sure the schema is up to date
+npm run update-schema
+if [[ $(git status -uno --porcelain) != "" ]]; then
+    echo "Review and commit the updated schema, then run this script again."
+    git status
+    exit 1
+fi
+
+# generate the next release version number
 LAST_VERSION=$(jq -r .version package.json)
 if [[ "$(git tag -l v$LAST_VERSION)" == "" ]]; then
     # the version in package.json was never released, so we assume this is the
@@ -25,8 +44,6 @@ if [[ "$PREVIOUS_RELEASE_SHA" != "" ]]; then
 fi
 git log --pretty=format:"%h %s" $GIT_RANGE | grep -oE "[0-9a-f]+ .*?\(#[0-9]+\)" | sed -E "s/[0-9a-f]+ /* /" | sed -E "s/\`/\`\`/g" | sed -E "s/\(#([0-9]+)\)/(\[#\1](https:\/\/github.com\/"$GIT_REPO"\/pull\/\1))/" > _log.md
 
-# edit release notes
-${EDITOR:-vi} _log.md
 head -n 2 CHANGES.md > _CHANGES.md
 echo "## $VERSION ($(date '+%Y-%m-%d'))" >> _CHANGES.md
 printf "\n" >> CHANGES.md
@@ -35,16 +52,20 @@ rm _log.md
 tail -n +2 CHANGES.md >> _CHANGES.md
 mv _CHANGES.md CHANGES.md
 
+# edit release notes
+${EDITOR:-vi} _log.md
+
+# update package version
 sed -i .bak 's/^  "version": ".*",$/  "version": "'$VERSION'",/' package.json
 rm package.json.bak
-npm run update-schema
 
 git diff
 read -p "Press ENTER to generate release $VERSION"
 
+# commit release changes
 git add package.json CHANGES.md src/schema.json
 git commit -m "Release $VERSION"
-git tag v$VERSION
+git tag -f v$VERSION
 
 echo "Release $VERSION is now ready!"
 echo "Next steps:"
