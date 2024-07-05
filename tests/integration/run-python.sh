8bb911a16b9e7f1c5ebc4cbf7cb90bf90c3f3733
Miguel Grinberg <miguel.grinberg@gmail.com>
2024-07-05 20:48:51 +0100

    select the correct Python client version for tests (Fixes #25) (#27)
    (cherry picked from commit 26d5e20953bbc7d4f427c0cc9431eeae32b9cad3)

--- tests/integration/run-python.sh
+++ tests/integration/run-python.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-BRANCH=$(git branch --show-current)
+BRANCH=$(jq -r .version package.json | grep -Eo "^[0-9]+\.[0-9]+")
 
 if [[ ! -d $SCRIPT_DIR/.venv ]]; then
     python -m venv $SCRIPT_DIR/.venv
+    echo "Installing from branch $BRANCH."
     $SCRIPT_DIR/.venv/bin/pip install https://github.com/elastic/elasticsearch-py/archive/$BRANCH.zip
     if [[ "$?" != "0" ]]; then
+        echo "Installation failure. Using main branch instead."
         $SCRIPT_DIR/.venv/bin/pip install https://github.com/elastic/elasticsearch-py/archive/main.zip
     fi
 fi
