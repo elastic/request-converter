e213a85493cdeb00916512470d42dd9ee66841c8
Josh Mock <josh@joshmock.com>
2024-07-24 16:16:06 -0500

    Install client in a more CI-friendly way

--- tests/integration/run-javascript.sh
+++ tests/integration/run-javascript.sh
@@ -5,18 +5,16 @@ set -eo pipefail
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 BRANCH=$(jq -r .version package.json | grep -Eo "^[0-9]+\.[0-9]+")
 
-if [[ ! -d "$SCRIPT_DIR/es-client" ]]; then
+if [[ ! -d "$SCRIPT_DIR/node_modules" ]]; then
   echo "Installing from branch $BRANCH."
-  git clone -b "$BRANCH" --depth=1 "https://github.com/elastic/elasticsearch-js.git" "$SCRIPT_DIR/es-client"
-  pushd "$SCRIPT_DIR/es-client"
+  test -d /tmp/es-client || git clone -b "$BRANCH" --depth=1 "https://github.com/elastic/elasticsearch-js.git" /tmp/es-client
+  pushd /tmp/es-client
   npm install
   npm run build
   popd
 
-  pushd "$SCRIPT_DIR"
   npm init -y
-  npm install "$SCRIPT_DIR/es-client"
-  popd
+  npm install /tmp/es-client
 fi
 
 if [[ "$1" != "" ]]; then
