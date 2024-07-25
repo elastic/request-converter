ee0ef99f823cd19075e655873d57eec70483e236
Josh Mock <josh@joshmock.com>
2024-07-25 13:57:42 -0500

    Bail on failing tests to get faster CI output
    Only while trying to fix this dumb issue with run-javascript.sh

--- scripts/test-format.sh
+++ scripts/test-format.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export ONLY_FORMAT=$1
-node --experimental-vm-modules node_modules/jest/bin/jest.js tests/integration/convert.test.ts
+node --experimental-vm-modules node_modules/jest/bin/jest.js --bail tests/integration/convert.test.ts
