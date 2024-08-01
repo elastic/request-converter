13fcb05c560d839f6ab15c9dc47874f3f943008d
Miguel Grinberg <miguel.grinberg@gmail.com>
2024-08-01 14:44:13 +0100

    logic error when deciding what JS client version to use

--- tests/integration/run-javascript.sh
+++ tests/integration/run-javascript.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -eo pipefail
+set -x
 
 SCRIPT_DIR=/tmp/js-test
 CLIENT_DIR=/tmp/es-client
@@ -9,8 +10,8 @@ BRANCH=$(jq -r .version package.json | grep -Eo "^[0-9]+\.[0-9]+")
 if [ ! -d "$CLIENT_DIR" ]; then
   echo "Installing from branch $BRANCH"
   git clone -b "$BRANCH" --depth=1 "https://github.com/elastic/elasticsearch-js.git" "$CLIENT_DIR" ||
-    echo "Branch $BRANCH not found. Cloning main branch." &&
-    git clone -b "main" --depth=1 "https://github.com/elastic/elasticsearch-js.git" "$CLIENT_DIR"
+    (echo "Branch $BRANCH not found. Cloning main branch." &&
+    git clone -b "main" --depth=1 "https://github.com/elastic/elasticsearch-js.git" "$CLIENT_DIR")
   pushd "$CLIENT_DIR"
   npm install
   npm run build
